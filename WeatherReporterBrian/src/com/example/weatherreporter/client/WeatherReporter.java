e91a08406202ecf9b416eb13ebd181e4ef903518
Brian Jordan <bcjordan@gmail.com>
2011-02-10 12:14:58 -0500

    Remove class files, further .gitignore improvements

--- WeatherReporterBrian/src/com/example/weatherreporter/client/WeatherReporter.java
+++ WeatherReporterBrian/src/com/example/weatherreporter/client/WeatherReporter.java
@@ -1,7 +1,5 @@
 package com.example.weatherreporter.client;
 
-import java.util.LinkedList;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.*;
